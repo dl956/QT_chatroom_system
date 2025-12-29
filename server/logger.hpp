3c5c7653bdd46d5c771a39341cd68aec2a96e164
dl956 <daweiminghan@163.com>
2025-12-29 22:39:29 +0800

    Refactor logger parameters and member variables

--- server/logger.hpp
+++ server/logger.hpp
@@ -14,17 +14,17 @@ public:
     static Logger& instance();
 
     // init must be called before first use (but instance() will create a usable default too)
-    void init(const std::string& file_path,
-              LogLevel level = LogLevel::Info,
+    void init(const std::string& log_file_path,
+              LogLevel log_level = LogLevel::Info,
               std::uint64_t max_size_bytes = 10ull * 1024 * 1024, // 10MB
               int rotate_count = 5);
 
-    void log(LogLevel lvl, const std::string& message, const nlohmann::json& extra = nlohmann::json());
+    void log(LogLevel log_level, const std::string& log_message, const nlohmann::json& extra = nlohmann::json());
 
-    void debug(const std::string& message, const nlohmann::json& extra = nlohmann::json());
-    void info(const std::string& message, const nlohmann::json& extra = nlohmann::json());
-    void warn(const std::string& message, const nlohmann::json& extra = nlohmann::json());
-    void error(const std::string& message, const nlohmann::json& extra = nlohmann::json());
+    void debug(const std::string& log_message, const nlohmann::json& extra = nlohmann::json());
+    void info(const std::string& log_message, const nlohmann::json& extra = nlohmann::json());
+    void warn(const std::string& log_message, const nlohmann::json& extra = nlohmann::json());
+    void error(const std::string& log_message, const nlohmann::json& extra = nlohmann::json());
 
 private:
     Logger();
@@ -32,15 +32,15 @@ private:
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
 
-    std::string level_to_string(LogLevel l) const;
+    std::string level_to_string(LogLevel log_level) const;
     std::string timestamp_iso() const;
     void rotate_if_needed_locked();
 
-    std::mutex mtx_;
-    std::ofstream ofs_;
-    std::string file_path_;
-    LogLevel level_;
-    std::uint64_t max_size_;
-    int rotate_count_;
-    bool initialized_;
+    std::mutex file_mutex_;
+    std::ofstream log_file_stream_;
+    std::string log_file_path_;
+    LogLevel log_level_;
+    std::uint64_t max_log_file_size_;
+    int log_rotate_count_;
+    bool is_initialized_;
 };
