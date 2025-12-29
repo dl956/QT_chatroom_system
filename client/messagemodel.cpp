93579091bbee9474babba635967ca2ce62f3f949
dl956 <daweiminghan@163.com>
2025-12-29 22:45:52 +0800

    Refactor MessageModel to use chat_items_

--- client/messagemodel.cpp
+++ client/messagemodel.cpp
@@ -1,36 +1,36 @@
-﻿#include "messagemodel.h"
+#include "messagemodel.h"
 #include <QDebug>
 
 MessageModel::MessageModel(QObject* parent) : QAbstractListModel(parent) {}
 
 int MessageModel::rowCount(const QModelIndex& parent) const {
     if (parent.isValid()) return 0;
-    return items_.size();
+    return chat_items_.size();
 }
 
 QVariant MessageModel::data(const QModelIndex& index, int role) const {
     if (!index.isValid()) return {};
-    const ChatItem& it = items_.at(index.row());
+    const ChatItem& chatItem = chat_items_.at(index.row());
     switch (role) {
-    case SenderRole: return it.sender;
-    case TextRole: return it.text;
-    case TimeRole: return it.time.toString(Qt::ISODate);
+    case SenderRole: return chatItem.sender;
+    case TextRole: return chatItem.text;
+    case TimeRole: return chatItem.time.toString(Qt::ISODate);
     }
     return {};
 }
 
 QHash<int, QByteArray> MessageModel::roleNames() const {
-    QHash<int, QByteArray> r;
-    r[SenderRole] = "sender";
-    r[TextRole] = "text";
-    r[TimeRole] = "time";
-    return r;
+    QHash<int, QByteArray> roleNameHash;
+    roleNameHash[SenderRole] = "sender";
+    roleNameHash[TextRole] = "text";
+    roleNameHash[TimeRole] = "time";
+    return roleNameHash;
 }
 
 void MessageModel::addMessage(const QString& sender, const QString& text, const QDateTime& time) {
     qDebug() << "[MessageModel] addMessage sender=" << sender << "text=" << text << "time=" << time.toString();
-    beginInsertRows(QModelIndex(), items_.size(), items_.size());
-    items_.append({sender, text, time});
+    beginInsertRows(QModelIndex(), chat_items_.size(), chat_items_.size());
+    chat_items_.append({sender, text, time});
     endInsertRows();
-    qDebug() << "[MessageModel] new rowCount=" << items_.size();
+    qDebug() << "[MessageModel] new rowCount=" << chat_items_.size();
 }
